93b5513c87f6f6523b2872d1084cdb6b49f5e9aa
Lester Lecong <lecong.lester@gmail.com>
2022-09-28 16:54:56 +0800

    Add BleInteractionPage

---
+++ BluetoothSample/Views/BleInteractionPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BluetoothSample.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BleInteractionPage : ContentPage
+    {
+        public BleInteractionPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
